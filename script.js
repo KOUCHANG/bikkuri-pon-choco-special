cd33e597ba3296cfa85c0ec1ac7b35089c3581e7
Kohei Ohtani <kohei_ohtani@dwango.co.jp>
2025-06-11 22:48:46 +0900

    imagesフォルダに新しい画像を追加し、スクリプトでの画像リストを更新

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ window.onload = function() {
     const images = [
         'images/image1.png',
         'images/image2.png',
+        'images/image3.png',
+        'images/image4.png',
+        'images/image5.png',
+        'images/image6.png',
     ];
 
     // --- 設定項目 ---
