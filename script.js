2e99738953acee55bdb629ed277f29c6e12345f5
Kohei Ohtani <kohei_ohtani@dwango.co.jp>
2025-06-13 16:03:35 +0900

    新しい画像「hasecchi.png」と「ripa_uchujin.png」、「tibiarichan.png」を追加し、画像の重み設定を更新

--- script.js
+++ script.js
@@ -6,33 +6,36 @@ window.onload = function() {
 
     // 🖼️ 画像のパスと重みを設定
     const images = [
-        { src: 'images/pon.png',               weight: 18 },
-        { src: 'images/kinako.png',            weight: 18 },
-        { src: 'images/misa.png',              weight: 18 },
-        { src: 'images/naruse.png',            weight: 12 },
+        { src: 'images/pon.png',               weight: 18  },
+        { src: 'images/kinako.png',            weight: 18  },
+        { src: 'images/misa.png',              weight: 18  },
+        { src: 'images/naruse.png',            weight: 12  },
 
-        { src: 'images/daibutsu_pon.png',      weight: 9  },
+        { src: 'images/koakuma_naruse.png',    weight: 4   },
 
-        { src: 'images/hiyoko.png',            weight: 95 },
+        { src: 'images/daibutsu_pon.png',      weight: 9   },
 
-        { src: 'images/kou.png',               weight: 36 },
-        { src: 'images/nahe.png',              weight: 36 },
+        { src: 'images/hiyoko.png',            weight: 110 },
 
-        { src: 'images/poni.png',              weight: 9  },
+        { src: 'images/kou.png',               weight: 36  },
+        { src: 'images/nahe.png',              weight: 36  },
+        { src: 'images/hasecchi.png',          weight: 36  },
 
-        { src: 'images/ripa.png',              weight: 9  },
-        { src: 'images/osta.png',              weight: 9  },
-        { src: 'images/totoro.png',            weight: 9  },
+        { src: 'images/poni.png',              weight: 9   },
+        { src: 'images/osta.png',              weight: 9   },
+        { src: 'images/totoro.png',            weight: 9   },
+        { src: 'images/tibiarichan.png',       weight: 9   },
 
-        { src: 'images/koakuma_naruse.png',    weight: 4  },
+        { src: 'images/ripa.png',              weight: 4   },
+        { src: 'images/ripa_uchujin.png',      weight: 5   },
 
-        { src: 'images/bravo.png',             weight: 8  },
-        { src: 'images/bravo_ban.png',         weight: 1  },
+        { src: 'images/bravo.png',             weight: 8   },
+        { src: 'images/bravo_ban.png',         weight: 1   },
 
-        { src: 'images/seseri_normal.png',     weight: 5  },
-        { src: 'images/seseri_rare.png',       weight: 3  },
-        { src: 'images/seseri_ultra_rare.png', weight: 1  },
-        { src: 'images/seseri_birthday.png',   weight: 1  },
+        { src: 'images/seseri_normal.png',     weight: 5   },
+        { src: 'images/seseri_rare.png',       weight: 3   },
+        { src: 'images/seseri_ultra_rare.png', weight: 1   },
+        { src: 'images/seseri_birthday.png',   weight: 1   },
     ];
 
     // ✨ パッケージ画像のパスを指定
