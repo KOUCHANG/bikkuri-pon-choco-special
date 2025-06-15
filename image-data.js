5f945ceb5490717df02bcf68f791143caf5cadbb
Kohei Ohtani <kohei_ohtani@dwango.co.jp>
2025-06-15 10:15:15 +0900

    画像データの重みを調整し、新しい画像ファイルを追加

--- image-data.js
+++ image-data.js
@@ -4,36 +4,40 @@ const allImagesData = [
     { src: 'images/misa.png',              weight: 12, description: '雷神みさ' },
     { src: 'images/naruse.png',            weight: 12, description: '穴なるせ' },
 
-    { src: 'images/kinako_gyaru.png',      weight: 6,  description: 'きなこ（レアバージョン）'  },
-    { src: 'images/misa_bigan.png',        weight: 6,  description: 'みさ（レアバージョン）'  },
-    { src: 'images/naruse_koakuma.png',    weight: 6,  description: '小悪魔なるせ'  },
+    { src: 'images/kinako_gyaru.png',      weight: 2,  description: 'きなこ（レアバージョン）1' },
+    { src: 'images/kinako_mama.png',       weight: 4,  description: 'きなこ（レアバージョン）2' },
 
-    { src: 'images/pon_daibutsu.png',      weight: 6,  description: '大仏ポン（本人）'  },
+    { src: 'images/misa_bigan.png',        weight: 6,  description: 'みさ（レアバージョン）' },
+
+    { src: 'images/naruse_koakuma.png',    weight: 3,  description: '小悪魔なるせ' },
+    { src: 'images/naruse_kimono.png',     weight: 3,  description: '着物なるせ' },
+
+    { src: 'images/pon_daibutsu.png',      weight: 6,  description: '大仏ポン（本人）' },
 
     { src: 'images/hiyoko.png',            weight: 18, description: '4人の奴隷ひよこ' },
-    { src: 'images/hiyoko_shinshi.png',    weight: 9,  description: '紳士にわとり'  },
+    { src: 'images/hiyoko_shinshi.png',    weight: 9,  description: '紳士にわとり' },
 
     { src: 'images/kou.png',               weight: 18, description: '平凡な犬こう' },
     { src: 'images/nahe.png',              weight: 18, description: 'なーへー' },
 
     { src: 'images/hasecchi.png',          weight: 18, description: 'はせっち' },
-    { src: 'images/hasecchi_rare.png',     weight: 3,  description: 'ちょっとレアなはせっち'  },
+    { src: 'images/hasecchi_rare.png',     weight: 3,  description: 'ちょっとレアなはせっち' },
 
-    { src: 'images/osta.png',              weight: 9,  description: 'Startend(&???)'  },
-    { src: 'images/totoro.png',            weight: 9,  description: 'ととろ'  },
-    { src: 'images/tibiarichan.png',       weight: 9,  description: 'ちびありさん'  },
+    { src: 'images/osta.png',              weight: 9,  description: 'Startend(&???)' },
+    { src: 'images/totoro.png',            weight: 9,  description: 'ととろ' },
+    { src: 'images/tibiarichan.png',       weight: 9,  description: 'ちびありさん' },
 
-    { src: 'images/poni.png',              weight: 9,  description: 'ぽにさん'  },
-    { src: 'images/poni_wanyudo.png',      weight: 6,  description: 'ぽにさん2'  },
+    { src: 'images/poni.png',              weight: 9,  description: 'ぽにさん1' },
+    { src: 'images/poni_wanyudo.png',      weight: 6,  description: 'ぽにさん2' },
 
-    { src: 'images/ripa.png',              weight: 5,  description: 'りぱさん'  },
-    { src: 'images/ripa_uchujin.png',      weight: 4,  description: 'りぱさん（レアバージョン）'  },
+    { src: 'images/ripa.png',              weight: 5,  description: 'りぱさん' },
+    { src: 'images/ripa_uchujin.png',      weight: 4,  description: 'りぱさん（レアバージョン）' },
 
-    { src: 'images/bravo.png',             weight: 8,  description: 'BRAVO'  },
-    { src: 'images/bravo_ban.png',         weight: 1,  description: 'BRAVO（激レアバージョン）'  },
+    { src: 'images/bravo.png',             weight: 8,  description: 'BRAVO' },
+    { src: 'images/bravo_ban.png',         weight: 1,  description: 'BRAVO（激レアバージョン）' },
 
-    { src: 'images/seseri_normal.png',     weight: 5,  description: 'せせり（ノーマル）'  },
-    { src: 'images/seseri_rare.png',       weight: 3,  description: 'せせり（レアバージョン）'  },
-    { src: 'images/seseri_ultra_rare.png', weight: 1,  description: 'せせり（ウルトラレアバージョン）'  },
-    { src: 'images/seseri_birthday.png',   weight: 1,  description: 'せせり（特別バージョン?）'  },
+    { src: 'images/seseri_normal.png',     weight: 5,  description: 'せせり（ノーマル）' },
+    { src: 'images/seseri_rare.png',       weight: 3,  description: 'せせり（レアバージョン）' },
+    { src: 'images/seseri_ultra_rare.png', weight: 1,  description: 'せせり（ウルトラレアバージョン）' },
+    { src: 'images/seseri_birthday.png',   weight: 1,  description: 'せせり（特別バージョン?）' },
 ];
